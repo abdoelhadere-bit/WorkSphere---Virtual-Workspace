d189832f39744c38afa1d46048054f27e6372893
Abderrahmane Elhadere <abdoelhadere3@gmail.com>
2025-11-19 11:18:40 +0100

    add an even lintener to annul btn to hide and reset the form container

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const ajoutBtn = document.querySelector(".ajoutBtn");
+const annulBtnForm = document.querySelector("#annulBtnForm");
 
 
 // Event on the ajout btn to display the form container
 ajoutBtn.addEventListener("click", () => {
     formContainerAjout.classList.remove("hidden");
     formContainerAjout.classList.add("flex");
+})
+
+// Event on the annul btn to hide and reset the form container
+annulBtnForm.addEventListener("click", () => {
+    formContainerAjout.classList.add("hidden");
+    formContainerAjout.classList.remove("flex");
+    cvForm.reset();
 })
