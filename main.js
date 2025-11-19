589517b9c947ea877394ba7f179e48c3677823bf
Abderrahmane Elhadere <abdoelhadere3@gmail.com>
2025-11-19 11:21:33 +0100

    add an even lintener to entire the document with conditions to specify the close btns

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const ajoutBtn = document.querySelector(".ajoutBtn");
 const annulBtnForm = document.querySelector("#annulBtnForm");
+const detailsModal = document.querySelector("#details");
+const employesChooseModal = document.querySelector("#employesChoose");
 
 
 // Event on the ajout btn to display the form container
@@ -13,4 +15,17 @@ annulBtnForm.addEventListener("click", () => {
     formContainerAjout.classList.add("hidden");
     formContainerAjout.classList.remove("flex");
     cvForm.reset();
+})
+
+//Add an event to document to close details and employeeChoose Modal
+document.addEventListener("click", (e) => {
+    // console.log(e.target)
+    if (e.target.classList.contains("closeDetails")) {
+        detailsModal.classList.add("hidden");
+        detailsModal.classList.remove("flex");
+    }
+    if (e.target.classList.contains("closeChoose")) {
+        employesChooseModal.classList.add("hidden");
+        employesChooseModal.classList.remove("flex");
+    }
 })
